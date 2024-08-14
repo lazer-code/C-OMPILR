6a7c982d3858b37ae27ab6833fbd9f9fad5d9c5a
YairMedina <99030228+DarknessRisesFromBelow@users.noreply.github.com>
2024-08-14 15:08:19 +0300

    added some things to test

--- Program.cs
+++ Program.cs
@@ -17,18 +17,29 @@
         newHead.AddChild(new Token(TokenType.Number, "28"));
         newHead.AddChild(new Token(TokenType.Identifier_var, "b"));
 
+        newHead = tt.AddChild(new Token(TokenType.Assignment, "="));
+        newHead.AddChild(new Token(TokenType.Number, "30"));
+        newHead.AddChild(new Token(TokenType.Identifier_var, "c"));
+
         
         newHead = tt.AddChild(new Token(TokenType.Keyword, "if"));
         var ifGreaterHead = newHead.AddChild(new Token(TokenType.GreaterThan, ">"));
-         ifGreaterHead.AddChild(new Token(TokenType.Identifier_var, "b"));
+        ifGreaterHead.AddChild(new Token(TokenType.Identifier_var, "b"));
         ifGreaterHead.AddChild(new Token(TokenType.Identifier_var, "a"));
         
         newHead = newHead.AddChild(new Token(TokenType.Body, ""));
-        var temp = newHead;
         newHead = newHead.AddChild(new Token(TokenType.Keyword, "return"));
         newHead.AddChild(new Token(TokenType.Number, "15"));
-
         
+        newHead = tt.AddChild(new Token(TokenType.Keyword, "if"));
+        ifGreaterHead = newHead.AddChild(new Token(TokenType.GreaterThan, ">"));
+        ifGreaterHead.AddChild(new(TokenType.Identifier_var, "c"));
+        ifGreaterHead.AddChild(new(TokenType.Identifier_var, "b"));
+
+        newHead = newHead.AddChild(new Token(TokenType.Body, ""));
+        newHead = newHead.AddChild(new Token(TokenType.Keyword, "return"));
+        newHead.AddChild(new Token(TokenType.Number, "17"));
+
         newHead = tt.AddChild(new Token(TokenType.Keyword, "return"));
         newHead.AddChild(new Token(TokenType.Number, "10"));
         tt.AddChild(new Token(TokenType.EOF, ""));
